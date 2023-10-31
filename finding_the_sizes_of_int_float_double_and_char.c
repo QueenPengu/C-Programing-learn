eab6e2dd5098af5f7b5258fa145d65f4244c033a
Okan Tümüklü <117488504+Okan-tumuklu@users.noreply.github.com>
2023-10-31 15:22:05 +0300

    Create finding_the_sizes_of_int_float_double_and_char.c

---
+++ finding_the_sizes_of_int_float_double_and_char.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+	
+	printf("İnt    size: %ld\n",sizeof(int));
+	printf("Float  size: %ld\n",sizeof(float));
+	printf("Double size: %ld\n",sizeof(double));
+	printf("Char   size: %ld\n",sizeof(char));
+	
+	return 0;
+	}
