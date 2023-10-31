8c33238cd53a432f90dd85a47c1b6333cf36fd06
Okan Tümüklü <117488504+Okan-tumuklu@users.noreply.github.com>
2023-10-31 15:18:22 +0300

    Create print_the_ascii_value_of_the_character.c

---
+++ print_the_ascii_value_of_the_character.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(void){
+	
+	char x;
+	printf("Please enter a letter:");
+	scanf("%c",&x);
+	printf("Ascii code of this letter: %d",x);
+	
+	
+	return 0;
+	}
