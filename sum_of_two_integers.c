1809a8762991b083a9566265b64029635ef46f32
Okan Tümüklü <117488504+Okan-tumuklu@users.noreply.github.com>
2023-10-31 15:13:36 +0300

    Create sum_of_two_integers.c

---
+++ sum_of_two_integers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void){
+	
+	int Number1,Number2,Sum;
+	
+	printf("Enter the 1st number:");
+	scanf("%d",&Number1);
+	printf("Enter the 2st number:");
+	scanf("%d",&Number2);
+	
+	Sum = Number1+Number2;
+	
+	printf("sum of numbers: %d \n",Sum);
+	
+	
+	return 0;
+	}
